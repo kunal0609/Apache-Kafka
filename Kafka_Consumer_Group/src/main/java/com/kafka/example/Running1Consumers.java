346912cf45f5f6d76b52ae21e1e3a6b0b923aba1
developers <developers@linux>
2020-10-29 18:33:50 +0530

    Commit first

---
+++ Kafka_Consumer_Group/src/main/java/com/kafka/example/Running1Consumers.java
@@ -0,0 +1,9 @@
+package com.kafka.example;
+
+public class Running1Consumers {
+
+	public static void main(String[] args) throws Exception {
+        ConsumerGroupExample.run(1, new String[]{"test-consumer-group"});
+    }
+	
+}
---
+++ Kafka_Consumer_Group/src/main/java/com/kafka/example/Running2Consumers.java
@@ -0,0 +1,14 @@
+package com.kafka.example;
+
+import java.util.Arrays;
+
+public class Running2Consumers {
+	
+	public static void main(String[] args) throws Exception {
+	
+	String[] consumerGroups = new String[2];
+    Arrays.fill(consumerGroups, "test-consumer-group");
+    ConsumerGroupExample.run(2, consumerGroups);
+}
+	
+}
